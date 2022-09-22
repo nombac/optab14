8a4aa1c422259def65ccf2d904f9ab877137fb66
Shigenobu Hirose <shirose@cavendish.yes.jamstec.go.jp>
2022-09-22 16:04:52 +0900

    initial commit in hybrid

---
+++ hybrid/hybrid.F90
@@ -0,0 +1,336 @@
+  
+#define OPACITY_DUST 0d0
+#define TMP_DUST 2.7d0
+
+#define SEMENOV_ROS '../Semenov/semenov_ros.data'
+#define SEMENOV_PLA '../Semenov/semenov_pla.data'
+#define FERGUSON_ROS '../Ferguson/ferguson_ros.data'
+#define FERGUSON_PLA '../Ferguson/ferguson_pla.data'
+#define OP_ROS '../OPCD_3.3/op_ros.data'
+#define OP_PLA '../OPCD_3.3/op_pla.data'
+#define DEPLETION 1d0
+
+  program hybrid
+    
+    implicit none
+    
+    real(8), parameter :: PI = acos(-1d0)
+    real(8), parameter :: RGAS = 8.31447d7
+    real(8), parameter :: MMW = 0.61d0
+    real(8), parameter :: GAMMA = 5d0/3d0
+
+    real(8) :: tmp, dtmp, tmp_min, tmp_max
+    real(8) :: tmp_ser, tmp_fer, tmp_sep, tmp_fep, tmp_opr, tmp_opp
+    real(8) :: tmpdsr, tmpdsp, tmpdfr, tmpdfp, tmpdor, tmpdop
+    real(8) :: rho, drho, rho_min, rho_max
+    real(8), allocatable :: opa_ros(:,:), opa_pla(:,:), dust(:,:)
+    real(8) :: opa_ros0, opa_pla0, f0r, f0p
+    real(8) :: opa_ros1, opa_pla1, f1r, f1p
+    real(8) :: opa_ros2, opa_pla2, f2r, f2p
+    real(8) :: opa_ros3, opa_pla3, f3r, f3p
+    integer :: i, imax
+    integer :: j, jmax
+    integer :: nt_ser, nd_ser, nt_sep, nd_sep
+    integer :: nt_fer, nd_fer, nt_fep, nd_fep
+    integer :: nt_opr, nd_opr, nt_opp, nd_opp
+    real(8), allocatable :: t_ser(:), d_ser(:), t_sep(:), d_sep(:)
+    real(8), allocatable :: ros_se(:,:), pla_se(:,:)
+    real(8), allocatable :: t_fer(:), d_fer(:), t_fep(:), d_fep(:)
+    real(8), allocatable :: ros_fe(:,:), pla_fe(:,:)
+    real(8), allocatable :: t_opr(:), d_opr(:), t_opp(:), d_opp(:)
+    real(8), allocatable :: ros_op(:,:), pla_op(:,:)
+    real(8) :: egas
+    REAL*8, PARAMETER :: temp_fe_op = 3.7d0
+    
+! READ SEMENOV OPACITIES
+    open(7, file=SEMENOV_ROS, form='unformatted', status='old')
+    read(7) nt_ser, nd_ser
+    allocate(t_ser(nt_ser), d_ser(nd_ser), ros_se(nt_ser,nd_ser))
+    read(7) t_ser, d_ser, ros_se
+    close(7)
+!    DO j = 1, nd_ser
+!       DO i = 1, nt_ser
+!          IF(ros_se(i,j)*0d0 .ne. 0d0 .and. t_ser(i) .lt. 2.9) THEN
+!             PRINT *, i,j, ros_se(i,j), t_ser(i)
+!          END IF
+!       END DO
+!    END DO
+!    stop
+    open(7, file=SEMENOV_PLA, form='unformatted', status='old')
+    read(7) nt_sep, nd_sep
+    allocate(t_sep(nt_sep), d_sep(nd_sep), pla_se(nt_sep,nd_sep))
+    read(7) t_sep, d_sep, pla_se
+    close(7)
+
+! READ FERGUSON OPACITIES    
+    open(7, file=FERGUSON_ROS, form='unformatted', status='old')
+    read(7) nt_fer, nd_fer
+    allocate(t_fer(nt_fer), d_fer(nd_fer), ros_fe(nt_fer,nd_fer))
+    read(7) t_fer, d_fer, ros_fe
+    close(7)
+    open(7, file=FERGUSON_PLA, form='unformatted', status='old')
+    read(7) nt_fep, nd_fep
+    allocate(t_fep(nt_fep), d_fep(nd_fep), pla_fe(nt_fep,nd_fep))
+    read(7) t_fep, d_fep, pla_fe
+    close(7)
+
+! READ OP OPACITIES    
+    open(7, file=OP_ROS, form='unformatted', status='old')
+    read(7) nt_opr, nd_opr
+    allocate(t_opr(nt_opr), d_opr(nd_opr), ros_op(nt_opr,nd_opr))
+    read(7) t_opr, d_opr, ros_op
+    close(7)
+    open(7, file=OP_PLA, form='unformatted', status='old')
+    read(7) nt_opp, nd_opp
+    allocate(t_opp(nt_opp), d_opp(nd_opp), pla_op(nt_opp,nd_opp))
+    read(7) t_opp, d_opp, pla_op
+    close(7)
+    
+    OPEN(55, FILE='temp_fe_op.data')
+    WRITE(55,*) temp_fe_op
+    CLOSE(55)
+
+    ! Rosseland mean
+    tmpdsr = 0.01d0
+    tmpdfr = 0.01d0
+    tmpdor = 0.02d0
+    tmp_ser = 2.0d0
+    tmp_fer = temp_fe_op
+    tmp_opr = 7.0d0
+
+    ! Planck mean
+    tmpdsp = 0.01d0
+    tmpdfp = 0.01d0
+    tmpdop = 0.02d0
+    tmp_sep = 2.0d0
+    tmp_fep = temp_fe_op
+    tmp_opp = 7.0d0
+
+    !temperature range
+    tmp_min =  0.5d0
+    tmp_max =    6d0
+    dtmp    =0.01d0
+
+    ! density range
+    rho_min = -22d0
+    rho_max =   0d0
+    drho    =0.04d0
+
+    imax = int((tmp_max - tmp_min) / dtmp) + 1
+    jmax = int((rho_max - rho_min) / drho) + 1
+    allocate(opa_ros(imax,jmax), opa_pla(imax,jmax), dust(imax,jmax))
+
+    do j = 1, jmax
+       rho = rho_min + drho * (j - 1)
+       do i = 1, imax
+          tmp = tmp_min + dtmp * (i - 1)
+          if(tmp < tmp_ser - tmpdsr) then
+             f0r = 1d0
+          else if(tmp < tmp_ser + tmpdsr) then
+             f0r = rightdown(tmp, tmp_ser, tmpdsr)
+          else
+             f0r = 0d0
+          end if
+          if(tmp < tmp_ser - tmpdsr) then
+             f1r = 0d0
+          else if(tmp < tmp_ser + tmpdsr) then
+             f1r = rightup(tmp, tmp_ser, tmpdsr)
+          else if(tmp < tmp_fer - tmpdfr) then
+             f1r = 1d0
+          else if(tmp < tmp_fer + tmpdfr) then
+             f1r = rightdown(tmp, tmp_fer, tmpdfr)
+          else
+             f1r = 0d0
+          end if
+          if(tmp < tmp_fer - tmpdfr) then
+             f2r = 0d0
+          else if(tmp < tmp_fer + tmpdfr) then
+             f2r = rightup(tmp, tmp_fer, tmpdfr)
+          else if(tmp < tmp_opr - tmpdor) then
+             f2r = 1d0
+          else if(tmp < tmp_opr + tmpdor) then
+             f2r = rightdown(tmp, tmp_opr, tmpdor)
+          else
+             f2r = 0d0
+          end if
+          if(tmp < tmp_opr - tmpdor) then
+             f3r = 0d0
+          else if(tmp < tmp_opr + tmpdor) then
+             f3r = rightup(tmp, tmp_opr, tmpdor)
+          else
+             f3r = 1d0
+          end if
+          
+          if(tmp < tmp_sep - tmpdsp) then
+             f0p = 1d0
+          else if(tmp < tmp_sep + tmpdsp) then
+             f0p = rightdown(tmp, tmp_sep, tmpdsp)
+          else
+             f0p = 0d0
+          end if
+          if(tmp < tmp_sep - tmpdsp) then
+             f1p = 0d0
+          else if(tmp < tmp_sep + tmpdsp) then
+             f1p = rightup(tmp, tmp_sep, tmpdsp)
+          else if(tmp < tmp_fep - tmpdfp) then
+             f1p = 1d0
+          else if(tmp < tmp_fep + tmpdfp) then
+             f1p = rightdown(tmp, tmp_fep, tmpdfp)
+          else
+             f1p = 0d0
+          end if
+          if(tmp < tmp_fep - tmpdfp) then
+             f2p = 0d0
+          else if(tmp < tmp_fep + tmpdfp) then
+             f2p = rightup(tmp, tmp_fep, tmpdfp)
+          else if(tmp < tmp_opp - tmpdop) then
+             f2p = 1d0
+          else if(tmp < tmp_opp + tmpdop) then
+             f2p = rightdown(tmp, tmp_opp, tmpdop)
+          else
+             f2p = 0d0
+          end if
+          if(tmp < tmp_opp - tmpdop) then
+             f3p = 0d0
+          else if(tmp < tmp_opp + tmpdop) then
+             f3p = rightup(tmp, tmp_opp, tmpdop)
+          else
+             f3p = 1d0
+          end if
+          opa_ros0 = opacity(tmp, rho, ros_se, t_ser, d_ser)
+          opa_ros1 = opacity(tmp, rho, ros_fe, t_fer, d_fer)
+          opa_ros2 = opacity(tmp, rho, ros_op, t_opr, d_opr)
+          opa_pla0 = opacity(tmp, rho, pla_se, t_sep, d_sep)
+          opa_pla1 = opacity(tmp, rho, pla_fe, t_fep, d_fep)
+          opa_pla2 = opacity(tmp, rho, pla_op, t_opp, d_opp)
+          
+          if(opa_ros0 > (OPACITY_DUST)) opa_ros1 = 0d0
+          if(opa_pla0 > (OPACITY_DUST)) opa_pla1 = 0d0
+
+          ! analytical expression for f-f + electron scattering
+          egas = RGAS/MMW/(GAMMA-1d0) * (10d0**rho) * (10d0**tmp)
+          opa_ros3 = log10(0.33d0 +10d52 * sqrt((10d0**rho)**9 / egas**7))
+          opa_pla3 = log10(        37d52 * sqrt((10d0**rho)**9 / egas**7))
+          
+          if(opa_ros0 > (OPACITY_DUST) .or. tmp < TMP_DUST) then
+             dust(i,j) = 1 ! DUST GRAINS EXIST
+             opa_ros(i,j) = opa_ros0! + opa_ros1 * f1r + opa_ros2 * f2r + opa_ros3 * f3r
+             opa_pla(i,j) = opa_pla0! + opa_pla1 * f1p + opa_pla2 * f2p + opa_pla3 * f3p
+          else
+             dust(i,j) = 0 ! DUST GRAINS SUBLIMATED
+             IF(opa_ros1*0d0 == 0d0 .and. opa_ros2*0d0 == 0d0) THEN
+                opa_ros(i,j) = opa_ros1 * f1r + opa_ros2 * f2r! + opa_ros3 * f3r
+             ELSE IF(opa_ros1*0d0 == 0d0 .and. opa_ros2*0d0 /= 0d0) THEN
+                opa_ros(i,j) = opa_ros1
+             ELSE IF(opa_ros1*0d0 /= 0d0 .and. opa_ros2*0d0 == 0d0) THEN
+                opa_ros(i,j) = opa_ros2
+             ELSE
+                opa_ros(i,j) = TRANSFER(-1_8, 0d0)
+             ENDIF
+             IF(opa_pla1*0d0 == 0d0 .and. opa_pla2*0d0 == 0d0) THEN
+                opa_pla(i,j) = opa_pla1 * f1r + opa_pla2 * f2r! + opa_pla3 * f3r
+             ELSE IF(opa_pla1*0d0 == 0d0 .and. opa_pla2*0d0 /= 0d0) THEN
+                opa_pla(i,j) = opa_pla1
+             ELSE IF(opa_pla1*0d0 /= 0d0 .and. opa_pla2*0d0 == 0d0) THEN
+                opa_pla(i,j) = opa_pla2
+             ELSE
+                opa_pla(i,j) = TRANSFER(-1_8, 0d0)
+             ENDIF
+          end if
+!!$          opa_ros(i,j) = opa_ros0
+!!$          opa_pla(i,j) = opa_pla0
+       end do
+    end do
+    
+    open(8, file='opacity.in')
+    write(8,*) imax, jmax
+    write(8,*) 10d0**tmp_min, 10d0**tmp_max
+    write(8,*) 10d0**rho_min, 10d0**rho_max
+    write(8,*) DEPLETION
+    close(8)
+    
+    open(8, file='kP.dat', form='unformatted')
+    write(8) 10d0**opa_pla
+    close(8)
+    open(8, file='kR.dat', form='unformatted')
+    write(8) 10d0**opa_ros
+    close(8)
+
+    open(8, file='dust.dat', form='unformatted')
+    write(8) dust
+    close(8)
+    
+  contains
+    
+    real(8) function opacity(tmp, rho, opa, tmp0, rho0)
+      real(8), intent(in) :: tmp
+      real(8), intent(in) :: rho
+      real(8), intent(in) :: opa(:,:)
+      real(8), intent(in) :: tmp0(:)
+      real(8), intent(in) :: rho0(:)
+      
+      integer :: nt, nd
+      integer :: it, id, it0, id0
+      real(8) :: op0, op1
+      
+      nt = size(tmp0)
+      nd = size(rho0)
+      
+      if(tmp <= tmp0(1)) then
+         opacity = -100d0
+         return
+      end if
+      if(tmp >= tmp0(nt)) then
+         opacity = -100d0
+         return
+      end if
+      
+      do it = 1, nt
+         if(tmp0(it) >= tmp) exit
+      end do
+      it0 = it
+
+      do id = 1, nd
+         if(rho0(id) >= rho) exit
+      end do
+      id0 = id
+
+      if(id0 == 1) then
+         op0 = opa(it0-1,1)
+         op1 = opa(it0  ,1)
+      else if(id0 == nd+1) then
+         op0 = opa(it0-1,nd)
+         op1 = opa(it0  ,nd)
+      else
+         op0 = ((rho - rho0(id0-1)) * opa(it0-1, id0) + (rho0(id0) - rho) * opa(it0-1, id0-1)) / (rho0(id0) - rho0(id0-1))
+         op1 = ((rho - rho0(id0-1)) * opa(it0  , id0) + (rho0(id0) - rho) * opa(it0  , id0-1)) / (rho0(id0) - rho0(id0-1))
+      end if
+      
+      opacity = ((tmp - tmp0(it0-1)) * op1 + (tmp0(it0) - tmp) * op0) /  (tmp0(it0) - tmp0(it0-1))
+
+      
+      return    
+    end function opacity
+    
+    real(8) function rightup(x, x0, d)
+      real(8), intent(in) :: x
+      real(8), intent(in) :: x0
+      real(8), intent(in) :: d
+      
+      rightup = 0.5d0 * (1d0 + sin(0.5d0 * PI * (x - x0) / d))
+      
+      return
+    end function rightup
+    
+    real(8) function rightdown(x, x0, d)
+      real(8), intent(in) :: x
+      real(8), intent(in) :: x0
+      real(8), intent(in) :: d
+      
+      rightdown = 0.5d0 * (1d0 - sin(0.5d0 * PI * (x - x0) / d))
+      
+      return
+    end function rightdown
+    
+  end program hybrid
+  
---
+++ hybrid/opacity_table.pro
@@ -0,0 +1,192 @@
+
+pro opacity_table
+
+  compile_opt idl2
+
+; initialize
+  PS_CLOSE
+  CLOSE, /all
+
+; component
+  component = ''
+
+; TEMPERATURE BORDER BETWEEN FERGUSON AND OP
+  OPENR, 1, 'temp_fe_op.data'
+  READF, 1, t_ferguson_max
+  CLOSE, 1
+
+; READ OPACITY TABLES
+  dir = './'
+  nidd = 0
+  nitt = 0
+  OPENR, 1, dir+'opacity.in'
+  READF, 1, nitt, nidd
+  READF, 1, topmin, topmax
+  READF, 1, dopmin, dopmax
+  CLOSE, 1
+  print, nitt, nidd
+  meanp = DBLARR(nitt,nidd)
+  meanr = DBLARR(nitt,nidd)
+  dust  = DBLARR(nitt,nidd)
+  OPENR, 1, dir+'kP.dat'+component, /f77_unformatted, /swap_endian
+  READU, 1, meanp
+  CLOSE, 1
+  OPENR, 1, dir+'kR.dat'+component, /f77_unformatted, /swap_endian
+  READU, 1, meanr
+  CLOSE, 1
+  OPENR, 1, dir+'dust.dat'+component, /f77_unformatted, /swap_endian
+  READU, 1, dust
+  CLOSE, 1
+
+; linear -> log
+  meanp = ALOG10(meanp)
+  meanr = ALOG10(meanr)
+  topmin = ALOG10(topmin)
+  topmax = ALOG10(topmax)
+  dopmin = ALOG10(dopmin)
+  dopmax = ALOG10(dopmax)
+
+; INDECIES OF SUBLIMATION TEMPERATURES
+  isubl = INTARR(nidd)
+  FOR i = 0, nidd-1 DO BEGIN
+;     FOR j = 0, nitt-1 DO BEGIN
+     FOR j = nitt-1, 0, -1 DO BEGIN
+        IF(dust[j,i] eq 1) THEN BREAK
+     ENDFOR
+     isubl[i] = j
+  ENDFOR
+
+  ; SLICE
+  IF(1) THEN BEGIN
+     d_slice = -6.
+     j_slice = 0
+     t = DBLARR(nitt)
+     FOR i = 0, nitt-1 DO BEGIN
+        t[i] = topmin + i * (topmax - topmin) / DOUBLE(nitt - 1)  
+     ENDFOR
+     FOR j = 0, nidd-1 DO BEGIN
+        d = dopmin + j * (dopmax - dopmin) / DOUBLE(nidd - 1)  
+        j_slice = j
+        IF(d GE d_slice) THEN BREAK
+     ENDFOR
+     !p.multi = [0,1,1]
+     PS_OPEN, 'slice', /color, ps_fonts='helvetica', /cmyk, /encapsulated, XSIZE=8.5, YSIZE=8.5/SQRT(2d0)
+     CGPLOT, t, t, xrange=[2.7,6.0], xs=1, yrange=[-3,6], ys=1, /NODATA, xtit=TEXTOIDL('log( T / K )'), ytit=TEXTOIDL('log( \kappa / cm^2g^{-1} )')
+     CGOPLOT, t, meanp[*,j_slice], thick=10, color='gray'
+     CGOPLOT, t, meanr[*,j_slice], thick=10
+     CGOPLOT, [3.75,4.1], [1.,1.+9*(4.1-3.75)], li=1, thick=3
+     CGOPLOT, [4.5,5.3], [4,4-3.5*(5.3-4.5)], li=1, thick=3
+     CGTEXT, 0.02, 0.02, TEXTOIDL('log\rho = '+STRTRIM(STRING(d_slice),2)), /NORM, CHARS=1
+     PS_CLOSE
+  ENDIF
+
+  
+  ; BORDER LINE (SEMENOV)
+  tsubl = DBLARR(nidd)
+  dsubl = DBLARR(nidd)
+  FOR i = 0, nidd-1 DO BEGIN
+     tsubl[i] = topmin + (topmax - topmin) / DOUBLE(nitt - 1) * isubl[i]
+     dsubl[i] = dopmin + (dopmax - dopmin) / DOUBLE(nidd - 1) * i
+  ENDFOR
+
+  ; BORDER LINE (OP)
+  OPENR, 1, '../OPCD_3.3/border.data'
+  READF, 1, jmax_op
+  rhomin_op = DBLARR(jmax_op)
+  rhomax_op = DBLARR(jmax_op)
+  temper_op = DBLARR(jmax_op)
+  FOR j = 0, jmax_op-1 DO BEGIN
+     dummy0 = 0d0
+     dummy1 = 0d0
+     dummy2 = 0d0
+     READF, 1, dummy0, dummy1, dummy2
+     temper_op[j] = dummy0
+     rhomin_op[j] = dummy1
+     rhomax_op[j] = dummy2
+  ENDFOR
+  CLOSE, 1
+
+  ; BORDER LINE (FERGUSON)
+  OPENR, 1, '../Ferguson/border.data'
+  READF, 1, jmax_fe
+  rhomin_fe = DBLARR(jmax_fe)
+  rhomax_fe = DBLARR(jmax_fe)
+  temper_fe = DBLARR(jmax_fe)
+  FOR j = 0, jmax_fe-1 DO BEGIN
+     dummy0 = 0d0
+     dummy1 = 0d0
+     dummy2 = 0d0
+     READF, 1, jdummy, dummy0, dummy1, dummy2
+     temper_fe[j] = dummy0
+     rhomin_fe[j] = dummy1
+     rhomax_fe[j] = dummy2
+  ENDFOR
+  CLOSE, 1
+
+  
+; xrange and yrange
+  xmin = topmin - (topmax - topmin) / FLOAT(nitt - 1) / 2
+  xmax = topmax + (topmax - topmin) / FLOAT(nitt - 1) / 2
+  ymin = dopmin - (dopmax - dopmin) / FLOAT(nidd - 1) / 2
+  ymax = dopmax + (dopmax - dopmin) / FLOAT(nidd - 1) / 2
+  xrange = [xmin,xmax]
+  yrange = [ymin,ymax]
+
+  imin = 0
+  imax = nitt-1
+  jmin = 0
+  jmax = nidd-1
+
+
+; floor and ceiling
+  value_max = 7.
+  value_min = -6.
+
+  meanp >= value_min
+  meanp <= value_max
+  meanr >= value_min
+  meanr <= value_max
+
+; convert from double to byte image
+  meanp = BYTE((meanp - value_min) / (value_max - value_min) * 255)
+  meanr = BYTE((meanr - value_min) / (value_max - value_min) * 255)
+
+
+; plot
+  !p.multi = [0,2,1]
+  psfile = 'opacity_table'+component
+  PS_OPEN, psfile, /color, ps_fonts='helvetica', /cmyk, /encapsulated, XSIZE=8.5, YSIZE=3.5
+
+  ; color
+  JHCOLORS
+
+  ; title
+  xtitle = TEXTOIDL('log T (K)')
+  ytitle = TEXTOIDL('log \rho (g cm^{-3})')
+
+  ; Rosseland-mean opacity
+  p = [0.2, 0.2, 0.9, 0.9]
+
+  CGIMAGE, meanr[imin:imax,jmin:jmax], /axes, xr=xrange, yr=yrange, pos=p, chars=1.0, axkey={xtit:xtitle,ytit:ytitle,tit:TEXTOIDL('log \kappa_R (cm^2g^{-1})')}, interp=0
+  CGCOLORBAR, /vertical, /right, pos=[p[2]+0.01, p[1], p[2]+0.02, p[3]], maxr=value_max, minr=value_min, chars=0.75
+  CGOPLOT, tsubl, dsubl, THICK=3, COL='WHITE', LIN=2
+  CGOPLOT, [t_ferguson_max, t_ferguson_max], [dopmin, dopmax], THICK=3, COL='WHITE', LIN=2
+  CGOPLOT, temper_op, rhomin_op, THICK=1, COL='WHITE', LIN=1
+  CGOPLOT, temper_op, rhomax_op, THICK=1, COL='WHITE', LIN=1
+  CGOPLOT, temper_fe, rhomin_fe, THICK=1, COL='WHITE', LIN=1
+  CGOPLOT, temper_fe, rhomax_fe, THICK=1, COL='WHITE', LIN=1
+  
+  ; Planck-mean opacity
+  p = [0.2, 0.2, 0.9, 0.9]
+  CGIMAGE, meanp[imin:imax,jmin:jmax], /axes, xr=xrange, yr=yrange, pos=p, chars=1.0, axkey={xtit:xtitle,ytit:ytitle,tit:TEXTOIDL('log \kappa_P (cm^2g^{-1})')}, interp=0
+  CGCOLORBAR, /vertical, /right, pos=[p[2]+0.01, p[1], p[2]+0.02, p[3]], maxr=value_max, minr=value_min, chars=0.75
+  CGOPLOT, tsubl, dsubl, THICK=3, COL='WHITE', LIN=2
+  CGOPLOT, [t_ferguson_max, t_ferguson_max], [dopmin, dopmax], THICK=3, COL='WHITE', LIN=2
+  CGOPLOT, temper_op, rhomin_op, THICK=1, COL='WHITE', LIN=1
+  CGOPLOT, temper_op, rhomax_op, THICK=1, COL='WHITE', LIN=1
+  CGOPLOT, temper_fe, rhomin_fe, THICK=1, COL='WHITE', LIN=1
+  CGOPLOT, temper_fe, rhomax_fe, THICK=1, COL='WHITE', LIN=1
+
+  PS_CLOSE
+
+END
